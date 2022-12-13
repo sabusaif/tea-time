7c4aba1f62e45ef98457bd7c420e8971ca9db0ab
Sarah <sarahsyouf02@gmail.com>
2022-12-13 12:50:57 -0800

    back-end code for showing the list of people the user had made a conversation with

---
+++ back-end/src/main/java/handler/GetFriendsHandler.java
@@ -0,0 +1,57 @@
+package handler;
+
+import dao.ConversationDao;
+import dao.UserDao;
+import dto.ConversationDto;
+import dto.MessageDto;
+import dao.MessageDao;
+import dto.UserDto;
+import handler.AuthFilter.AuthResult;
+import org.bson.Document;
+import request.ParsedRequest;
+import response.HttpResponseBuilder;
+import response.RestApiAppResponse;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GetFriendsHandler implements BaseHandler {
+
+    @Override
+    public HttpResponseBuilder handleRequest(ParsedRequest request) {
+        ConversationDao conversationDao = ConversationDao.getInstance();
+        UserDao userDao = UserDao.getInstance();
+        MessageDao messageDao = MessageDao.getInstance();
+        AuthResult authResult = AuthFilter.doFilter(request);
+        if(!authResult.isLoggedIn){
+            return new HttpResponseBuilder().setStatus(StatusCodes.UNAUTHORIZED);
+        }
+
+        // gets the user that pressed the "Show Friends" button
+        String currentUser = request.getQueryParam("currentUser");
+
+        var filter = new Document("userName", currentUser);
+
+        List<ConversationDto> convo = conversationDao.query(filter);
+        Iterator<ConversationDto> check = convo.iterator();
+
+        List<UserDto> friends = new ArrayList<>();
+
+        while (check.hasNext()) {
+            String message = check.next().getConversationId();
+            String[] split = message.split("_");
+            // will add the other user to the list
+            if (split[0].equals(currentUser)) {
+                friends.add(userDao.query(new Document("userName", split[1])).get(0));
+            } else {
+                friends.add(userDao.query(new Document("userName", split[0])).get(0));
+            }
+        }
+
+        // returns list of people the user has been in a conversation with
+        var res = new RestApiAppResponse<>(true, friends, null);
+        return new HttpResponseBuilder().setStatus("200 OK").setBody(res);
+    }
+
+}
--- back-end/src/main/java/handler/GetMessagesHandler.java
+++ back-end/src/main/java/handler/GetMessagesHandler.java
@@ -20,6 +20,7 @@ public class GetMessagesHandler implements BaseHandler {
     public HttpResponseBuilder handleRequest(ParsedRequest request) {
         MessageDao messageDao = MessageDao.getInstance();
         AuthResult authResult = AuthFilter.doFilter(request);
+
         if(!authResult.isLoggedIn){
             return new HttpResponseBuilder().setStatus(StatusCodes.UNAUTHORIZED);
         }
@@ -29,15 +30,19 @@ public class GetMessagesHandler implements BaseHandler {
         List<MessageDto> convo = messageDao.query(filter);
         Iterator<MessageDto> check = convo.iterator();
 
+        // this is what the user had searched
         String search = request.getQueryParam("search");
 
         while (check.hasNext()) {
             String message = check.next().getMessage();
+            // not case sensitive
             if (!message.toUpperCase().contains(search.toUpperCase())) {
+                // if it isn't the same, the message will be removed from the list
                 check.remove();
             }
         }
 
+        // returns list of messages that included the search word
         var res = new RestApiAppResponse<>(true, convo, null);
         return new HttpResponseBuilder().setStatus("200 OK").setBody(res);
     }
